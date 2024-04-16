52f64e0b081ed887d3b8ff51f7ed0d5abc42ee82
Jan <131482887+jangmz@users.noreply.github.com>
2024-04-16 14:47:50 +0200

    added default values

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { GeneralInfo } from './components/generalInfo.jsx'
+import { Education } from './components/education.jsx'
 //import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <GeneralInfo />
+      <GeneralInfo firstName="John" lastName="Doe" email="johndoe@dev.com" phone="111222333" />
+      <Education />
     </>
   )
 }
