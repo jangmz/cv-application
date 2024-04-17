139670ce4f468807062e9738b12a7d5a8ef47582
Jan <131482887+jangmz@users.noreply.github.com>
2024-04-17 11:52:20 +0200

    refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
 import { useState } from 'react'
 import { GeneralInfo } from './components/generalInfo.jsx'
 import { Education } from './components/education.jsx'
+import { Experience } from './components/experience.jsx'
+import { Preview } from './components/preview.jsx'
 //import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
+  const [generalInfoData, setGeneralInfoData] = useState([]);
+  const [educationData, setEducationData] = useState([]);
+  const [experienceData, setExperienceData] = useState([]);
 
   return (
     <>
-      <GeneralInfo firstName="John" lastName="Doe" email="johndoe@dev.com" phone="111222333" />
-      <Education />
+      <div className="inputFields">
+        <GeneralInfo 
+          firstName=""
+          lastName=""
+          email=""
+          phone=""
+          onUpdate={setGeneralInfoData} 
+        />
+        <Education 
+          schoolName=""
+          title=""
+          startDate=""
+          endDate=""
+          ongoing={false}
+          onUpdate={setEducationData} 
+        />
+        <Experience 
+          company=""
+          position=""
+          responsibilities=""
+          startDate=""
+          endDate=""
+          ongoing={false}
+          onUpdate={setExperienceData}
+        />
+      </div>
+      <div className="preview">
+        <h1>Preview</h1>
+        <Preview 
+          generalInfoData={generalInfoData}
+          educationData={educationData}
+          experienceData={experienceData}
+        />
+      </div>
     </>
   )
 }
